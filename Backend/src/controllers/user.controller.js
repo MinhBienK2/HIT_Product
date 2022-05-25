a786f790a1fdbbc27872b260cee16b0e5e7702f2
MinhBienK2 <phamminhbien333@gmail.com>
2022-05-25 21:35:48 +0700

    update refactoring backend

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,3 +1,14 @@
-// const { User } = require("../models");
+const ApiError = require("../utils/ApiError");
+const CatchAsync = require("../utils/CatchAsync");
+const { User } = require("../models");
 
-// module.exports = {};
+// CRUD User
+const getAllUsers = CatchAsync((req, res, next) => {});
+
+const getUser = CatchAsync((req, res, next) => {});
+
+const updateUser = CatchAsync((req, res, next) => {});
+
+const deleteUser = CatchAsync((req, res, next) => {});
+
+module.exports = { getAllUsers, getUser, updateUser, deleteUser };
--- Backend/src/routes/v1/index.js
+++ Backend/src/routes/v1/index.js
@@ -1,1 +1,19 @@
-module.exports.userRoute = require("./user.route");
+const express = require("express");
+const router = express.Router();
+
+const userRoute = require("./user.route");
+const authRoute = require("./auth.route");
+const { route } = require("./user.route");
+
+const defaultRoutes = [
+    {
+        path: "/users",
+        route: userRoute,
+    },
+];
+
+defaultRoutes.forEach((route) => {
+    router.use(route.path, route.route);
+});
+
+module.exports = router;
