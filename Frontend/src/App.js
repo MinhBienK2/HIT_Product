7d39f78f8ca2c20c2c04d10cfce17746dcf012e1
aabhotu <nguyenthithuy18012002@gmail.com>
2022-06-02 14:26:42 +0700

    ref#8 base

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { publicRoutes } from './routes/router';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { publicRoutes } from "./routes/router";
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        const Layout = route.component;
-                        return <Route key={index} path={route.path} element={<Layout />} />;
-                    })}
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          {publicRoutes.map((route, index) => {
+            const Layout = route.component;
+            return <Route key={index} path={route.path} element={<Layout />} />;
+          })}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
