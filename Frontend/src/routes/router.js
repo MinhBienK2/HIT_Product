2f4c7be5a6adc6174925faed535f7b10890c331f
aabhotu <nguyenthithuy18012002@gmail.com>
2022-06-02 23:17:51 +0700

    ref#9#10 signin signup

--- Frontend/src/routes/router.js
+++ Frontend/src/routes/router.js
@@ -2,12 +2,16 @@ import Home from "../pages/home/home";
 import Login from "../pages/login/Login";
 import ForgetPass from "../pages/forgetPass/ForgetPass";
 import Signup from "../pages/signup/Signup";
+import Choice from "../pages/resetPass/choice/Choice";
+import Verify from "../pages/resetPass/verify/Verify";
 
 const publicRoutes = [
   { path: "/", component: Login },
-  { path: "/Register", component: ForgetPass },
+  { path: "/ForgetPass", component: ForgetPass },
   { path: "/Home", component: Home },
   { path: "/Signup", component: Signup },
+  { path: "/Choice", component: Choice },
+  { path: "/Verify", component: Verify },
 ];
 
 const privateRoutes = [];
