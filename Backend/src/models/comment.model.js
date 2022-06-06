9dd3934513e515592f84342161ea11bae772d606
t-onion <t.onion.2002@gmail.com>
2022-06-06 16:14:15 +0700

    commit to pull code

--- Backend/src/models/comment.model.js
+++ Backend/src/models/comment.model.js
@@ -5,7 +5,8 @@ const commentSchema = new Schema({
    content: String,
    parentCmt: { type: Schema.Types.ObjectId, ref: "Comments", default: null },
    postID: { type: Schema.Types.ObjectId, ref: "Posts", default: null, required: true}
-}, {
+}, 
+{
    timestamps: true,
 });
 
