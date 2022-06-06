1b30f01bc1eb4e13514720b1d048bd85320a7d86
t-onion <t.onion.2002@gmail.com>
2022-06-06 15:57:44 +0700

    refs#5 comments Handle

--- Backend/src/models/comment.model.js
+++ Backend/src/models/comment.model.js
@@ -4,12 +4,12 @@ const { Schema } = mongoose
 const commentSchema = new Schema({
    content: String,
    parentCmt: { type: Schema.Types.ObjectId, ref: "Comments", default: null },
-   postID: { type: Schema.Types.ObjectId, ref: "Posts", default: null }
+   postID: { type: Schema.Types.ObjectId, ref: "Posts", default: null, required: true}
 }, {
    timestamps: true,
 });
 
-const Comment = mongoose.model("Comments", postSchema)
+const Comment = mongoose.model("Comments", commentSchema)
 
 module.exports = Comment
 
