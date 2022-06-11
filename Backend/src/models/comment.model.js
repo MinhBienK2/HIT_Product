b3549bf7690d56b306c66ccdb2cf5b413c81babf
t-onion <t.onion.2002@gmail.com>
2022-06-12 00:49:35 +0700

    ref#5 repair logic comment

--- Backend/src/models/comment.model.js
+++ Backend/src/models/comment.model.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose")
 const { Schema } = mongoose
+const ApiError = require("../utils/ApiError");
+
 
 const commentSchema = new Schema({
    content: String,
    parentCmt: { type: Schema.Types.ObjectId, ref: "Comments", default: null },
-   postID: { type: Schema.Types.ObjectId, ref: "Posts", default: null, required: true}
+   postID: { type: Schema.Types.ObjectId, ref: "Posts", default: null, required: true},
+   author: { type: Schema.Types.ObjectId, ref: "Users", default: null},
 }, 
 {
    timestamps: true,
 });
+
 
 const Comment = mongoose.model("Comments", commentSchema)
 
--- Backend/src/services/featureCRUD.service.js
+++ Backend/src/services/featureCRUD.service.js
@@ -2,6 +2,7 @@ const ApiError = require("../utils/ApiError");
 const CatchAsync = require("../utils/CatchAsync");
 const { sendMail } = require("./sendMail.service");
 const { Post } = require("../models");
+const { Comment } = require("../models");
 
 const getAllModel = (Model) =>
     CatchAsync(async (req, res, next) => {
@@ -31,26 +32,49 @@ const createModel = (Model) =>
     CatchAsync(async (req, res, next) => {
         let data;
         if (Model === Post) {
+            console.log("run post");
             const files = req.files.photos
             const filenames = (files) ? files.map(file => file.filename) : ""
-            data = await Model.create({
+            const data = await Model.create({
                 description: req.body.description,
                 shareOf: req.body.shareOf,
                 author: req.user._id,
                 photos: filenames
             });
         }
-        else {
-            console.log(req.body);
-            data = await Model.create(req.body);
+        else if (Model === Comment) {
+            let isCreate = false;
+            if (req.body.parentCmt) {
+                const check = await Comment.findById(req.body.parentCmt);
+                if (!check)
+                    return next(new ApiError(`parentCmt does not exist`, 404));
+                else if(check.postID != req.body.postID) 
+                    return next(new ApiError(`parentCmt does not math`, 403));
+                else
+                    isCreate = true;
+            }else
+                isCreate = true;
+            if (isCreate) {
+                console.log("run");
+                data = await Comment.create({
+                content: req.body.content,
+                postID: req.body.postID,
+                parentCmt: req.body.parentCmt,
+                author: req.user._id,
+            })
+            }            
+        } else {
+            data = await Model.create(req.body);  
         }
         if (!data) {
-            next(new ApiError(`create fail`, 404));
+            return next(new ApiError(`create fail`, 404));
+        } else {
+            res.status(200).json({
+                status: "success",
+                data,
+            });
         }
-        res.status(200).json({
-            status: "success",
-            data,
-        });
+        
     });
 
 const updateModel = (Model) =>
@@ -66,16 +90,24 @@ const updateModel = (Model) =>
             }
             await data.save()
         }
+        else if (Model === Comment) {
+            if (req.body.postID || req.body.parentCmt)
+                return next(new ApiError(`only for updating content field`, 403));
+            data = await Model.findByIdAndUpdate(req.params.id, req.body);           
+        }
         else {
             data = await Model.findByIdAndUpdate(req.params.id, req.body);
         }
         if (!data) {
             next(new ApiError(`create fail`, 404));
         }
-        res.status(200).json({
-            status: "success",
-            data,
-        });
+        else {
+
+            res.status(200).json({
+                status: "success",
+                data,
+            });
+        }
     });
 
 const deleteModel = (Model) =>
