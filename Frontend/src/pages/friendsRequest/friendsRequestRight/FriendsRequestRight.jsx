80fa64ca2b0c14c75a5960a82eaa45041f3edb4e
aabhotu <nguyenthithuy18012002@gmail.com>
2022-08-06 22:46:30 +0700

    listFriend

--- Frontend/src/pages/friendsRequest/friendsRequestRight/FriendsRequestRight.jsx
+++ Frontend/src/pages/friendsRequest/friendsRequestRight/FriendsRequestRight.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {useNavigate} from 'react-router-dom'
 import './FriendsRequestRight.scss'
 import FriendsRequestItems from './friendRequestItems/FriendRequestItems'
+import Axios from "axios";
 
 function FriendsRequestRight() {
+
+  const [confirmFriend, setConfirmFriend] = useState([]);
+    useEffect(() => {
+        Axios({
+            method: "GET",
+            url: `http://localhost:3000/api/v1/friends/list-friend-confirm`,
+            withCredentials: true,
+        })
+            .then((data) => {
+                console.log(data);
+                if (
+                    data.status === "success" &&
+                    data.lisConfirmFriend.length !== 0
+                ) {
+                    data.lisConfirmFriend.forEach((ele) => {
+                        console.log(ele);
+                        setConfirmFriend((current) => [...current, ele]);
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    async function handleAccess(ele, friendId) {
+        try {
+            const data = await Axios({
+                method: "PATCH",
+                url: `http://localhost:3000/api/v1/friends/${friendId}`,
+                withCredentials: true,
+            });
+            if (data.status === "success") {
+                setConfirmFriend((current) => {
+                    return current.filter((ele2) => {
+                        return ele2 !== ele;
+                    });
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const navigate = useNavigate()
+
+    async function handleAbort(ele, friendId) {
+        try {
+            const data = await Axios({
+                method: "DELETE",
+                url: `http://localhost:3000/api/v1/friends/${friendId}`,
+                withCredentials: true,
+            });
+            if (data.status === "success") {
+                setConfirmFriend((current) => {
+                    return current.filter((ele2) => {
+                        return ele2 !== ele;
+                    });
+                });
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    console.log(confirmFriend);
+
   return (
     <div className='friendsRequestRight'>
         <div className="friendsRequestRight_header">
@@ -10,6 +79,7 @@ function FriendsRequestRight() {
             <p>Xem tất cả</p>
         </div>
         <div className="friendsRequestRight_main">
+            {/* <FriendsRequestItems />
             <FriendsRequestItems />
             <FriendsRequestItems />
             <FriendsRequestItems />
@@ -18,8 +88,16 @@ function FriendsRequestRight() {
             <FriendsRequestItems />
             <FriendsRequestItems />
             <FriendsRequestItems />
-            <FriendsRequestItems />
-            <FriendsRequestItems />
+            <FriendsRequestItems /> */}
+            {confirmFriend && confirmFriend.map((ele) => {
+              return (
+                <FriendsRequestItems
+                  key={ele._id}
+                  avatar={ele.friendId.avatar}
+                  name = {ele.friendId.name}
+                />
+              )
+            })}
         </div>
     </div>
   )
