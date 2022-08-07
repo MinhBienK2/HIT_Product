8dc0cd1ab4f8fd9a229d083b276d528ab7d77e47
MinhBienK2 <phamminhbien333@gmail.com>
2022-08-07 14:37:04 +0700

    update late

--- Frontend/src/pages/friendsRequest/friendsRequestRight/friendRequestItems/FriendRequestItems.jsx
+++ Frontend/src/pages/friendsRequest/friendsRequestRight/friendRequestItems/FriendRequestItems.jsx
@@ -1,121 +1,122 @@
 import React, { useState, useEffect } from "react";
-import './FriendRequestItems.scss'
-import img1 from '../../../../assets/images/Frame 212.svg'
-import img2 from '../../../../assets/images/Frame 213.svg'
-import img3 from '../../../../assets/images/Frame 214.svg'
+import "./FriendRequestItems.scss";
+import img1 from "../../../../assets/images/Frame 212.svg";
+import img2 from "../../../../assets/images/Frame 213.svg";
+import img3 from "../../../../assets/images/Frame 214.svg";
 import { Avatar } from "@mui/material";
 import Axios from "../../../../services/axios.service";
 
-function FriendRequestItems({key, avatar, name}) {
+function FriendRequestItems({ keyId, avatar, name }) {
+    const [confirmFriend, setConfirmFriend] = useState([]);
 
-  const [confirmFriend, setConfirmFriend] = useState([]);
+    useEffect(() => {
+        Axios({
+            method: "GET",
+            url: `http://localhost:3000/api/v1/friends/list-friend-confirm`,
+            withCredentials: true,
+        })
+            .then((data) => {
+                console.log(data);
+                if (
+                    data.status === "success" &&
+                    data.lisConfirmFriend.length !== 0
+                ) {
+                    data.lisConfirmFriend.forEach((ele) => {
+                        console.log(ele);
+                        setConfirmFriend((current) => [...current, ele]);
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
 
-  useEffect(() => {
-    Axios({
-        method: "GET",
-        url: `http://localhost:3000/api/v1/friends/list-friend-confirm`,
-        withCredentials: true,
-    })
-        .then((data) => {
-            console.log(data);
-            if (
-                data.status === "success" &&
-                data.lisConfirmFriend.length !== 0
-            ) {
-                data.lisConfirmFriend.forEach((ele) => {
-                    console.log(ele);
-                    setConfirmFriend((current) => [...current, ele]);
+    async function handleAccess(ele, friendId) {
+        try {
+            const data = await Axios({
+                method: "PATCH",
+                url: `http://localhost:3000/api/v1/friends/${friendId}`,
+                withCredentials: true,
+            });
+            if (data.status === "success") {
+                setConfirmFriend((current) => {
+                    return current.filter((ele2) => {
+                        return ele2 !== ele;
+                    });
                 });
             }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-}, []);
-
-  async function handleAccess(ele, friendId) {
-    try {
-        const data = await Axios({
-            method: "PATCH",
-            url: `http://localhost:3000/api/v1/friends/${friendId}`,
-            withCredentials: true,
-        });
-        if (data.status === "success") {
-            setConfirmFriend((current) => {
-                return current.filter((ele2) => {
-                    return ele2 !== ele;
-                });
-            });
+        } catch (error) {
+            console.log(error);
         }
-    } catch (error) {
-        console.log(error);
     }
-}
 
-  async function handleAbort(ele, friendId) {
-    try {
-        const data = await Axios({
-            method: "DELETE",
-            url: `http://localhost:3000/api/v1/friends/${friendId}`,
-            withCredentials: true,
-        });
-        if (data.status === "success") {
-            setConfirmFriend((current) => {
-                return current.filter((ele2) => {
-                    return ele2 !== ele;
-                });
+    async function handleAbort(ele, friendId) {
+        try {
+            const data = await Axios({
+                method: "DELETE",
+                url: `http://localhost:3000/api/v1/friends/${friendId}`,
+                withCredentials: true,
             });
+            if (data.status === "success") {
+                setConfirmFriend((current) => {
+                    return current.filter((ele2) => {
+                        return ele2 !== ele;
+                    });
+                });
+            }
+        } catch (error) {
+            console.log(error);
         }
-    } catch (error) {
-        console.log(error);
     }
-}
-  console.log('t',confirmFriend)
+    console.log("t", confirmFriend);
 
-  return (
-    <div>
-      {confirmFriend && confirmFriend.map((ele) => {
-        return (
-          <div className="friendsRequestItems">
-            <Avatar src={avatar}/>
-            <h3>{name}</h3>
-            <div className="friendsRequestItems_bbc">
-                <div className="friendsRequestItems_bbc-left">
-                    <img className="friendsRequestItems_bbc-left-img1" src={img2} alt="" />
-                    <img className="friendsRequestItems_bbc-left-img2" src={img3} alt="" />
-                </div>
-                <div className="friendsRequestItems_bbc-right">
-                    <p>50 bạn bè chung</p>
-                </div>
-            </div>
-            <button 
-              className="friendsRequestItems-btn1"
-              onClick={() => {
-                handleAccess(
-                  ele,
-                  ele.userId.id
-                );
-                }}
-            >
-                Chấp nhận
-              </button>
-            <button 
-              className="friendsRequestItems-btn2"
-              onClick={() => {
-                handleAbort(
-                  ele,
-                  ele.userId.id
-                );
-              }}
-            >
-              Từ chối
-            </button>
-          </div>
-        )
-      })} 
-    </div>
-    
-  )
+    return (
+        <div>
+            {confirmFriend &&
+                confirmFriend.map((ele) => {
+                    return (
+                        <div className="friendsRequestItems">
+                            <Avatar src={avatar} />
+                            <h3>{name}</h3>
+                            <div className="friendsRequestItems_bbc">
+                                <div className="friendsRequestItems_bbc-left">
+                                    <img
+                                        className="friendsRequestItems_bbc-left-img1"
+                                        src={img2}
+                                        alt=""
+                                    />
+                                    <img
+                                        className="friendsRequestItems_bbc-left-img2"
+                                        src={img3}
+                                        alt=""
+                                    />
+                                </div>
+                                <div className="friendsRequestItems_bbc-right">
+                                    <p>50 bạn bè chung</p>
+                                </div>
+                            </div>
+                            <button
+                                className="friendsRequestItems-btn1"
+                                onClick={() => {
+                                    handleAccess(ele, ele.userId.id);
+                                }}
+                            >
+                                Chấp nhận
+                            </button>
+                            <button
+                                className="friendsRequestItems-btn2"
+                                onClick={() => {
+                                    handleAbort(ele, ele.userId.id);
+                                }}
+                            >
+                                Từ chối
+                            </button>
+                        </div>
+                    );
+                })}
+        </div>
+    );
 }
 
-export default FriendRequestItems
+export default FriendRequestItems;
--- Frontend/src/pages/moreFriends/friendsRequestRight/FriendsRequestRight.jsx
+++ Frontend/src/pages/moreFriends/friendsRequestRight/FriendsRequestRight.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FriendsRequestItems from "./friendRequestItems/FriendRequestItems";
+import Axios from "../../../services/axios.service";
 
 function FriendsRequestRight() {
+    const [confirmFriend, setConfirmFriend] = useState([]);
+    useEffect(() => {
+        Axios({
+            method: "GET",
+            url: `http://localhost:3000/api/v1/users/list-user-make-friend`,
+            withCredentials: true,
+        })
+            .then((data) => {
+                if (
+                    data.status === "success" &&
+                    data.listUserMakeFriend.length !== 0
+                ) {
+                    data.listUserMakeFriend.forEach((ele) => {
+                        setConfirmFriend((current) => [...current, ele]);
+                    });
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
     return (
         <div className="friendsRequestRight">
             <div className="friendsRequestRight_header">
@@ -9,16 +32,17 @@ function FriendsRequestRight() {
                 <p>Xem tất cả</p>
             </div>
             <div className="friendsRequestRight_main">
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
-                <FriendsRequestItems />
+                {confirmFriend &&
+                    confirmFriend.map((ele) => {
+                        return (
+                            <FriendsRequestItems
+                                keyId={ele.id}
+                                avatar={ele.avatar}
+                                name={ele.name}
+                                ele={ele}
+                            />
+                        );
+                    })}
             </div>
         </div>
     );
