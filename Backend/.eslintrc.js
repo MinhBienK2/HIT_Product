7401b5df873e2f9a2c9aadaa281505a6115da5ea
MinhBienK2 <phamminhbien333@gmail.com>
2023-09-28 02:33:28 +0700

    chore: add prettier

--- Backend/.eslintrc.js
+++ Backend/.eslintrc.js
@@ -1,32 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const prettierOptions = JSON.parse(fs.readFileSync(path.resolve(__dirname, '.prettierrc'), 'utf8'));
+
 module.exports = {
-    "env": {
+    env: {
         "browser": true,
         "es2021": true,
         "node": true
     },
-    "extends": "eslint:recommended",
-    "overrides": [
+    plugins: ['prettier'],
+    extends: ["eslint:recommended","prettier"],
+    overrides: [
         {
             "env": {
                 "node": true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
+            files: ['**/*.ts?(x)'],
             "parserOptions": {
                 "sourceType": "script"
-            }
+            },
+            rules: { 'prettier/prettier': ['warn', prettierOptions] },
         }
     ],
-    "parserOptions": {
+    parserOptions: {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
-    "rules": {
+    rules: {
+        'prettier/prettier': ['error', prettierOptions],
         '@typescript-eslint/interface-name-prefix': 'off',
         '@typescript-eslint/explicit-function-return-type': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
-        semi: ["error", "always"]
+        semi: ["error", "always"],
+        //'no-unused-vars': ['error', { vars: 'all' }], //error when variable not used
       },
 };
